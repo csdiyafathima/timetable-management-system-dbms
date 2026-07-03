38057012f77baa976f0474f0aea89c1ac2e1f0e8
csdiyafathima <csdiyafathima@gmail.com>
2026-07-03 11:00:52 +0530

    changed Timetable Management System to Timetable Management

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -19,7 +19,7 @@ $currentTime = date("H:i:s");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard - Timetable Management System</title>
+    <title>Admin Dashboard - Timetable Management </title>
     <style>
         * {
             margin: 0;
